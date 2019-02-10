dd0a354649d029ccdeeae09ac85ec781a04b9d93
THE LUPA <32226207+vlad2456@users.noreply.github.com>
2019-02-10 17:59:31 +0400

    Update kill.js

--- js/kill.js
+++ js/kill.js
@@ -52,8 +52,8 @@
         }
           window.setInterval(doInverseSpinZeroPitch, 500);
     /*-------------------------------------------------------------------------------------------------------------------------------------------------- */
-    var tKillNames = ["THELUPA", "THELUPA1488", "CHAMPION"];
-    var ctKillNames = ["Ben Swolo", "Merry Legs", "00dat", "Ur mum", "Lansage", "Kokkir", "Ge.rig", "Micqua", "Me"];
+    var tKillNames = ["THELUPA", "THELUPA1488"];
+    var ctKillNames = ["Ben Swolo 1337", "Samsonome", "YuRka", "Adya", "TANYUHA", "ҜåẑẏҝᎥ", "KoTuK", "Fittnes_Trainer", "THELUPA1448","THELUPA"];
     var weapons = ["ssg", "deserteagle"];
     var $killFeedContainer = $('.kill-feed');
     var $killFeedElement = $('.kill-feed > div').hide();
